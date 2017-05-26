6da1fb80047b88af2a08d18ab4b37e528c364274
Mika <Mika.Skjelnes.6139@student.uu.se>
2017-05-26 19:54:14 +0200

    falldamage script
    Determines how much the player is damaged upon landing after a fall. 
    Fall damage is printed in the console.

---
+++ scripts/fallDmg.py
@@ -0,0 +1,9 @@
+import bge
+import bpy
+controller = bge.logic.getCurrentController()
+scene = bge.logic.getCurrentScene()
+obj = controller.owner
+linVz = obj.getLinearVelocity(True)[2] #gets z-velocity
+
+if linVz < (-10): #down is negative
+    print("You're hit with ",int(linVz*linVz*2)," points of falldamage")
