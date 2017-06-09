7b6ad7e3996c9d7156c10d6ffdc39cd53b9d6fca
Mika <Mika.Skjelnes.6139@student.uu.se>
2017-06-10 00:56:49 +0200

    updated camPos
    revamped camera controls

--- scripts/camPos.py
+++ scripts/camPos.py
@@ -2,13 +2,26 @@ import bge
 cont = bge.logic.getCurrentController()
 own = cont.owner
 scene = bge.logic.getCurrentScene()
+keyboard = cont.sensors["keyDist"]
 #see blender file for actual placement of these objects.
 empty = scene.objects["empty"]
 reset = scene.objects["reset"]
+
+cam = scene.objects["3rdcam"]
+playerPoint = scene.objects["player_coll"]
 #ray sensor is connected to "fpscam"
 ray = cont.sensors["ray"]
+
+for key,status in keyboard.events:
+    if status == bge.logic.KX_INPUT_JUST_ACTIVATED:
+        if key == bge.events.WHEELUPMOUSE or key == bge.events.WHEELDOWNMOUSE:
+            calcRange()
+
+def calcRange():
+    ray.range = (cam.worldPosition - playerPoint.worldPosition).length
+    print(ray.range)
 #Camera position depending on ray output.
 if ray.positive:
     empty.worldPosition = ray.hitPosition
 else:
-    empty.worldPosition = empty.worldPosition    
+    empty.worldPosition = reset.worldPosition    
